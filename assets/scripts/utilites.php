54ce149d3272fb518890f829a59442e2406546f7
Adauto Moisés <74927238+adautomoises@users.noreply.github.com>
2022-04-02 23:48:28 -0300

    utilites refatored

--- assets/scripts/utilites.php
+++ assets/scripts/utilites.php
@@ -1,28 +1,25 @@
 <?php
-  class turmas_m{
+  class turmas{
     public $link;
   }
+
   $turmas_m = array("action=getAlunosTurma&ano=20211&escola=1&grau_serie=3&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=4&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=5&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=11&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=12&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=13&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=14&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=15&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=16&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=17&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=17&turno=M&turma=2&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=18&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=19&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=21&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=22&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=23&turno=M&turma=1&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=24&turno=M&turma=1&status=C");
   for ($i=0; $i < count($turmas_m); $i++) { 
-    $turma_m[$i] = new turmas_m();
+    $turma_m[$i] = new turmas();
     $turma_m[$i]->link = $turmas_m[$i];
   }
 
-  class turmas_t{
-    public $link;
-  }
+  
   $turmas_t = array("action=getAlunosTurma&ano=20211&escola=1&grau_serie=3&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=4&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=5&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=11&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=12&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=13&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=14&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=15&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=16&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=17&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=18&turno=T&turma=2&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=19&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=21&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=22&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=23&turno=T&turma=3&status=C","action=getAlunosTurma&ano=20211&escola=1&grau_serie=24&turno=T&turma=3&status=C");
   for ($i=0; $i < count($turmas_t); $i++) { 
-    $turma_t[$i] = new turmas_t();
+    $turma_t[$i] = new turmas();
     $turma_t[$i]->link = $turmas_t[$i];
   }
-  class turmas_n{
-    public $link;
-  }
+
   $turmas_n = array("action=getAlunosTurma&ano=20211&escola=1&grau_serie=31&turno=N&turma=1&status=C");
 
   for ($i=0; $i < count($turmas_n); $i++) { 
-    $turma_n[$i] = new turmas_n();
+    $turma_n[$i] = new turmas();
     $turma_n[$i]->link = $turmas_n[$i];
   }
 ?>
